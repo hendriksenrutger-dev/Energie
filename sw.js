217e6c535e089eb97b85313ef449171a02e0b5a9
hendriksenrutger-dev <hendriksen.rutger@gmail.com>
2026-05-03 12:36:20 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,34 @@
-const CACHE = 'energie-inzicht-v1';
+// Energie Inzicht - Service Worker
+// Versie wordt automatisch bijgewerkt bij elke deploy
+
+const CACHE = 'energie-v2';
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(['./']))
-  );
+  // Direct activeren zonder te wachten op oude clients
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
+  // Verwijder alle oude caches
   e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+      Promise.all(keys.map(k => caches.delete(k)))
     )
   );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
+  // Network first: altijd proberen de nieuwste versie te laden
+  // Alleen als het netwerk faalt, gebruik de cache
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
-      const clone = res.clone();
-      caches.open(CACHE).then(c => c.put(e.request, clone));
-      return res;
-    }))
+    fetch(e.request)
+      .then(res => {
+        // Sla op in cache voor offline gebruik
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
   );
 });

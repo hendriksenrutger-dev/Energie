17f5cca42b4ccda444117e4b894b74f1c6cecd5e
hendriksenrutger-dev <hendriksen.rutger@gmail.com>
2026-05-03 11:35:30 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,18 @@
 const CACHE = 'energie-inzicht-v1';
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./'])));
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(['./']))
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
   self.clients.claim();
 });
 
